9156e0ca20175ff434e6e8236b794df72e78cb02
OyasumiiAlex <b181010@unach.mx>
2024-02-15 23:09:05 -0600

    test(feature): change content function
    version 1.3.5-alpha

--- multistepForm/main.js
+++ multistepForm/main.js
@@ -17,6 +17,15 @@ function isValidInput(){
 botonC.addEventListener('click', isValidInput);
 
 /*2do script (Añadir funcionalidad de transicion de ventanas)*/
+function getButtonClick(){
+    /*Obtener el nodo y cambiar su clase*/
+    const stepOne = document.getElementById('step1');
+    const stepTwo = document.getElementById('step2');
+    stepOne.classList.remove('active');
+    stepTwo.classList.add('active');
+}
+//Cuando se presione el boton se manda a llama la clase getButton...
+botonC.addEventListener('click', getButtonClick);
 /*3er script (Agregar efecto de pasos de página)*/
 
 
